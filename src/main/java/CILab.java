2dcd574c141818da974f584c772c230aa35b6d1b
Gikwon Jeong <jkwon100@gmail.com>
2023-06-09 18:12:42 -0700

    added travis and code implementation

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -1,3 +1,6 @@
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+
 public class CILab implements CILabInterface {
     private String myString;
 
@@ -13,8 +16,28 @@ public class CILab implements CILabInterface {
 
     @Override
     public boolean detectCapitalUse() {
+        if (isLowerCase(myString.charAt(0))) {
+            for (int i = 0; i < myString.length(); i++) {
+                if (isUpperCase(myString.charAt(i)))
+                    return false;
+            }
+            return true;
+        }
+        else {
+            if(isLowerCase(myString.charAt(1))) {
+                for (int i = 1; i < myString.length(); i++) {
+                    if (isUpperCase(myString.charAt(i)))
+                        return false;
+                }
+            }
+            else {
+                for (int i = 1; i < myString.length(); i++) {
+                    if (isLowerCase(myString.charAt(i)))
+                        return false;
+                }
+            }
+        }
         return false;
     }
-
 }
 
